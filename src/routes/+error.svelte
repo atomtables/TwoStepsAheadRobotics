e2c23d8fcad942ffa83740b6907a2d263d114486
Adithiya Venkatakrishnan <76572470+atomtables@users.noreply.github.com>
2024-11-09 16:44:39 -0500

    404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<div style="display: flex; height: 100vh; justify-content: center; align-items: center">
+    <div style="display: flex; flex-direction: column;">
+        <h1 style="color: white; font-family: 'Inter',sans-serif">
+            Oopsies! An error occured.
+        </h1>
+        <button style="background-color: white; border: none; border-radius: 10px; padding: 10px; font-family: 'Inter', sans-serif;">
+            <a href="/TwoStepsAheadRobotics">Return to the light</a>
+        </button>
+    </div>
+</div>
